2751d38c26de8f2909766f2b8d02c8f208876a78
Casey Duckering <cduckering@gmail.com>
2023-02-26 04:25:14 -0500

    Fix extras install to raster

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires = [
     ],
     extras_require = {
-        'render': [
+        'raster': [
             'cairoSVG~=2.3',
             'numpy~=1.16',
             'imageio~=2.5',
