5e11115ee7b663dcff8bb83bd0f73ee74cad197a
Casey Duckering <cduckering@gmail.com>
2023-03-05 01:58:52 -0500

    Fix svg tag indent

--- drawsvg/drawing.py
+++ drawsvg/drawing.py
@@ -12,7 +12,7 @@ from . import (
 
 XML_HEADER = '<?xml version="1.0" encoding="UTF-8"?>\n'
 SVG_START = ('<svg xmlns="http://www.w3.org/2000/svg" '
-             'xmlns:xlink="http://www.w3.org/1999/xlink"\n   ')
+             'xmlns:xlink="http://www.w3.org/1999/xlink"\n    ')
 SVG_END = '</svg>'
 SVG_CSS_FMT = '<style>/*<![CDATA[*/{}/*]]>*/</style>'
 SVG_JS_FMT = '<script>/*<![CDATA[*/{}/*]]>*/</script>'
